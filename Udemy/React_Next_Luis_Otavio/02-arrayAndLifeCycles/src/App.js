1888436d65dc0dcfda40d73001cc81fff66578dd
ailsonjr <ailsonjr21@gmail.com>
2022-06-24 15:32:05 -0300

    Adding external data with fetch and initial css

--- Udemy/React_Next_Luis_Otavio/02-arrayAndLifeCycles/src/App.js
+++ Udemy/React_Next_Luis_Otavio/02-arrayAndLifeCycles/src/App.js
@@ -3,59 +3,83 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    counter: 0,
+    posts: []
+
+    /*counter: 0,
     posts: [
       {
         id: 1,
-        tittle: 'Tittle 1',
+        title: 'Title 1',
         body: 'Body 1'
       },
       {
         id: 2,
-        tittle: 'Tittle 2',
+        title: 'Title 2',
         body: 'Body 2'
       },
       {
         id: 3,
-        tittle: 'Tittle 3',
+        title: 'Title 3',
         body: 'Body 3'
       }
-    ]
+    ]*/
   };
-  timeoutUpdate = null;
+  // timeoutUpdate = null;
 
   componentDidMount() {
-    this.handleTimeout();
-  }
+    this.loadPosts();
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    //   .then(response => response.json())
+    //   .then(posts => this.setState({ posts }));
+    // this.handleTimeout();
+  };
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos });
+  };
 
   componentDidUpdate() {
-    this.handleTimeout();
-  }
+    // this.handleTimeout();
+  };
 
   componentWillUnmount() {
-    clearTimeout(this.timeoutUpdate);
-  }
+    // clearTimeout(this.timeoutUpdate);
+  };
 
-  handleTimeout() {
+  /*handleTimeout() {
     const { posts, counter } = this.state;
-    posts[0].tittle = 'The tittle change';
+    posts[0].title = 'The title change';
     this.timeoutUpdate = setTimeout(() => {
       this.setState({ posts, counter: counter + 1 })
     }, 2000)
-  }
+  }*/
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
     return (
-      <div className="App">
-        <h1>{counter}</h1>
-        {posts.map(post => (
-          <div key={post.id}>
-            <h1>{post.tittle}</h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className='container'>
+        <div className="posts">
+          {posts.map(post => (
+            <div className='post'>
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className='post-content'>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
